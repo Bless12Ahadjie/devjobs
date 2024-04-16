3b2a5ab05453c23a2924ffb1a95f1ba1a0044963
Bless Ahadjie <89154198+Bless12Ahadjie@users.noreply.github.com>
2024-04-16 15:36:53 +0000

    feat-card componenet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { FilterComponent } from './shared/components/filter/filter.component';
+import { CardComponent } from './shared/components/card/card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,HeaderComponent,FilterComponent],
+  imports: [CommonModule, RouterOutlet,HeaderComponent,FilterComponent,CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
