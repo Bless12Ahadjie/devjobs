da61f1eff0978c645499ba74b519511d26e62792
Bless Ahadjie <89154198+Bless12Ahadjie@users.noreply.github.com>
2024-04-17 11:02:34 +0000

    initialized  job types for the jobs array

---
+++ src/app/core/Types/Types.ts
@@ -0,0 +1,21 @@
+export interface job {
+    id: number,
+    company: string,
+    logo: string, 
+    logoBackground: string,
+    position: string,
+    postedAt: string,
+    contract: string,
+    location:   string,
+    website: string,
+    apply: string,
+    description: string,
+    requirements:{
+        content: string,
+        items: Array<string>,
+    },
+    role:{
+        content: string,
+        items: Array<string>,
+    }
+}
