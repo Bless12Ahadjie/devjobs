2c568a5a8444f6c06d49ff2bd1defa332132dd48
Bless Ahadjie <89154198+Bless12Ahadjie@users.noreply.github.com>
2024-04-17 11:25:24 +0000

    added routing to page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { JobdetailsComponent } from './shared/layouts/jobdetails/jobdetails.component';
+import { ApplayoutComponent } from './shared/pages/applayout/applayout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {'path': '' , component: ApplayoutComponent},
+    {'path': 'details' , component: JobdetailsComponent},
+];
---
+++ src/app/shared/layouts/jobdetails/jobdetails.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'devjobs-jobdetails',
+  standalone: true,
+  imports: [],
+  templateUrl: './jobdetails.component.html',
+  styleUrl: './jobdetails.component.css'
+})
+export class JobdetailsComponent {
+
+}
