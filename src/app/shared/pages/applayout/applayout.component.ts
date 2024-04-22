da0ebe8c761915d5b77bcc8951d91a944833221c
Bless Ahadjie <89154198+Bless12Ahadjie@users.noreply.github.com>
2024-04-22 19:26:06 +0000

    path fixes after getting rid of filter component

--- src/app/shared/pages/applayout/applayout.component.ts
+++ src/app/shared/pages/applayout/applayout.component.ts
@@ -1,5 +1,4 @@
 import { Component, ViewChild } from '@angular/core';
-import { FilterComponent } from '../../components/filter/filter.component';
 import { CardbodyComponent } from '../../layouts/cardbody/cardbody.component';
 import { ButtonComponent } from '../../components/button/button.component';
 import { CommonModule } from '@angular/common';
@@ -7,7 +6,7 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'devjobs-applayout',
   standalone: true,
-  imports: [CardbodyComponent, FilterComponent, ButtonComponent, CommonModule],
+  imports: [CardbodyComponent, ButtonComponent, CommonModule],
   templateUrl: './applayout.component.html',
   styleUrls: ['./applayout.component.css']
 })
