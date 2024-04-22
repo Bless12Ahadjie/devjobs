2b8beeb4af037a2626e382f7a38a596a688235ec
Bless Ahadjie <89154198+Bless12Ahadjie@users.noreply.github.com>
2024-04-22 19:00:29 +0000

    implementing filter logic using behaviorSubject

---
+++ src/app/core/services/filter/filter.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import data from '../../data/data.json';
+import { job } from '../../Types/Types';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FilterService {
+  private jobData: job[] = [];
+  public filteredJobsSource = new BehaviorSubject<job[]>([]);
+  filteredJobs$ = this.filteredJobsSource.asObservable();
+  title = '';
+  location = '';
+  isFullTime = false;
+
+  constructor() {
+    this.jobData = data;
+    this.filteredJobsSource.next(this.jobData);
+  }
+
+  filterJobsBySearch(searchTerm: string): void {
+    this.title = searchTerm;
+    this.applyFilters();
+  }
+
+  filterJobsByLocation(location: string): void {
+    this.location = location;
+    this.applyFilters();
+  }
+
+  filterJobsByContract(isFullTime: boolean): void {
+    this.isFullTime = isFullTime;
+    this.applyFilters();
+  }
+
+  private applyFilters(): void {
+    let filteredJobs = this.jobData;
+  
+    if (this.title) {
+      filteredJobs = filteredJobs.filter(job =>
+        job.position.toLowerCase().includes(this.title.toLowerCase()) ||
+        job.company.toLowerCase().includes(this.title.toLowerCase())
+      );
+    }
+  
+    if (this.location) {
+      filteredJobs = filteredJobs.filter(job => job.location.toLowerCase().includes(this.location.toLowerCase()));
+    }
+  
+    if (this.isFullTime) {
+      filteredJobs = filteredJobs.filter(job => job.contract === 'Full Time');
+    }
+  
+    this.filteredJobsSource.next(filteredJobs);
+  }
+}
