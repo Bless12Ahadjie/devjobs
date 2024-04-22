35c9b2a08249f245f96ceffbc4bce042a0fde273
Bless Ahadjie <89154198+Bless12Ahadjie@users.noreply.github.com>
2024-04-22 15:32:45 +0000

    added skeloton loading

--- src/app/shared/layouts/cardbody/cardbody.component.ts
+++ src/app/shared/layouts/cardbody/cardbody.component.ts
@@ -3,11 +3,14 @@ import { CardComponent } from '../../components/card/card.component';
 import data from '../../../core/data/data.json';
 import { job } from '../../../core/Types/Types';
 import { CommonModule, Location } from '@angular/common';
+import { Router, NavigationExtras, ActivatedRoute } from '@angular/router';
+import { SkeletonComponent } from '../../components/skeleton/skeleton.component';
+
 
 @Component({
   selector: 'devjobs-cardbody',
   standalone: true,
-  imports: [CardComponent,CommonModule],
+  imports: [CardComponent, CommonModule,SkeletonComponent],
   templateUrl: './cardbody.component.html',
   styleUrls: ['./cardbody.component.css']
 })
@@ -15,43 +18,48 @@ export class CardbodyComponent implements OnInit {
   jobs: job[] = [];
   maxItemsPerPage = 12;
   currentPage: number = 1;
+  isLoading =true;
 
-  constructor(private location: Location) {}
+  constructor(private location: Location, private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit() {
-    this.currentPage = this.getInitialPage();
-    this.loadJobs();
-    // this.navigateBack();
+    this.route.queryParams.subscribe(params => {
+      this.currentPage = params['page'] ? +params['page'] : 1;
+      this.loadJobs();
+    });
   }
-
   loadJobs() {
     const startIndex = (this.currentPage - 1) * this.maxItemsPerPage;
     const endIndex = startIndex + this.maxItemsPerPage;
     this.jobs = data.slice(startIndex, endIndex);
+    setTimeout(()=> {
+      this.isLoading = false;
+    }, 2000);
+    
   }
 
   loadMore() {
     this.currentPage++;
     this.saveCurrentPage();
     this.loadJobs();
-  }
-
-  navigateBack() {
-    if (this.currentPage > 1) {
-      this.currentPage--;
-      this.saveCurrentPage();
-      this.loadJobs();
-    } else {
-      this.location.back();
-    }
-  }
-
-  private getInitialPage(): number {
-    const storedPage = localStorage.getItem('currentPage');
-    return storedPage ? parseInt(storedPage) : 1;
+    this.updateUrlWithPageParam();
+ 
   }
 
   private saveCurrentPage() {
     localStorage.setItem('currentPage', this.currentPage.toString());
   }
+
+  private updateUrlWithPageParam() {
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: { page: this.currentPage },
+      queryParamsHandling: 'merge'
+    });
+  }
+
+  filterFullTime() {
+    return this.jobs.filter(job => job.contract.includes('Full Time'));
+  }
+
 }
