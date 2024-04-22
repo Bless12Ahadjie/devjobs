2682012c74f234354d4245a2ca684cdb9bfabd33
Bless Ahadjie <89154198+Bless12Ahadjie@users.noreply.github.com>
2024-04-22 15:33:18 +0000

    wraped my text when width is met with an ellipse

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { JobdetailsComponent } from './shared/pages/jobdetails/jobdetails.component';
 import { ApplayoutComponent } from './shared/pages/applayout/applayout.component';
+import { SkeletonComponent } from './shared/components/skeleton/skeleton.component';
 
 export const routes: Routes = [
     {'path': '' , component: ApplayoutComponent},
