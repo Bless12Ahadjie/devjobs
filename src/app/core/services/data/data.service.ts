a705010b92d5fef1825597f46feb487f7d90e0b3
Bless Ahadjie <89154198+Bless12Ahadjie@users.noreply.github.com>
2024-04-22 18:59:42 +0000

    changed data service path and separated filter service

---
+++ src/app/core/services/data/data.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import data from '../../data/data.json';
+import { job } from '../../Types/Types';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, map, BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  constructor(private route: ActivatedRoute) {}
+
+  private jobData: job[] = [];
+
+
+  setJobData(data: job[]): void {
+    this.jobData = data;
+  }
+
+ public getJobData(): job[] {
+    return this.jobData;
+  }
+
+ public getJobDetails(id: number): job {
+    return data[id - 1];
+  }
+
+ public getRouteParam(paramName: string): Observable<number> {
+    return this.route.paramMap.pipe(
+      map(params => {
+        return Number(params.get(paramName));
+      })
+    );
+  }
+
+  
+}
