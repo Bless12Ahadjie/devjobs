6e33228ec6ba74e432a89015b205546729f53bfc
Bless Ahadjie <89154198+Bless12Ahadjie@users.noreply.github.com>
2024-05-10 10:16:29 +0000

    unit test for data service

--- src/app/core/services/data/data.service.spec.ts
+++ src/app/core/services/data/data.service.spec.ts
@@ -1,12 +1,57 @@
 import { TestBed } from '@angular/core/testing';
-
 import { DataService } from './data.service';
+import { Job } from '../../Types/Types';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { of } from 'rxjs';
+import jobdata from '../../data/data.json';
 
 describe('DataService', () => {
   let service: DataService;
+  let jobData: Job[];
+  let activatedRouteMock: any;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    activatedRouteMock = {
+      paramMap: of({
+        get: (param: string) => '1'
+      } as ParamMap)
+    };
+
+    TestBed.configureTestingModule({
+      providers: [
+        DataService,
+        { provide: ActivatedRoute, useValue: activatedRouteMock }
+      ]
+    });
     service = TestBed.inject(DataService);
+  });
+
+  it('should correctly set data to job data', () => {
+    const data: Job[] = [
+      {
+        id: 1,
+        position: 'Software Engineer',
+        company: 'Amalitech',
+        location: 'New York',
+        contract: 'Full Time',
+      },
+      // Add more job data as needed
+    ];
+    service.setJobData(data);
+    expect(service.getJobData()).toEqual(data);
+  });
+
+
+
+
+
+
+
+  it('should get route param as an observable', () => {
+    const testParam = 'id';
+
+    service.getRouteParam(testParam).subscribe((result) => {
+      expect(result).toEqual(1);
+    });
   });
 });
